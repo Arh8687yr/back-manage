b6fe227a6eb532b95e9996cb0217d43e83f023fa
Arh8687yr <1250696730@qq.com>
2019-12-27 10:54:54 +0800

    退出登录操作

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-button type="info" @click="handleOut">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  methods: {
+    handleOut() {
+      this.$confirm("您确定退出？").then(() => {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 };
 </script>
 
